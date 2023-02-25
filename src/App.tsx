e2b3213496371199bb9eaeab7d81a232a0eedbd5
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-02-25 19:11:58 -0300

    [FEATURE]: Add tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react'
 
 function App (): ReactElement {
   return (
-    <div className='App'>
+    <div className='h-screen bg-red-600'>
       <h1>Notion Avatar Generator</h1>
     </div>
   )
