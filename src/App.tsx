fc8afe1f6d122e539c8dc34d0ff50420beb92e8c
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-02-25 18:56:28 -0300

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react'
+
+function App (): ReactElement {
+  return (
+    <div className='App'>
+      <h1>Notion Avatar Generator</h1>
+    </div>
+  )
+}
+
+export default App
