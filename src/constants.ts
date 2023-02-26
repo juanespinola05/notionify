681cc40f5098564341ed64564825c103c2559aab
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-02-26 02:15:20 -0300

    [FEATURE]: Create .env and environment constants

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const ENVIRONMENT = {
+  CLOUD_NAME: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME ||= '',
+  PRESET_ID: import.meta.env.VITE_CLOUDINARY_PRESET_ID ||= '',
+  API_BASE_URL: import.meta.env.VITE_CLOUDINARY_API_BASE_URL ||= ''
+}
