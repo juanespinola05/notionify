67c02709289e7f802707a36be625afa0d72e9e9a
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-02-26 12:54:52 -0300

    [FEATURE]: Add util to map details

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { ReactElement } from 'react'
+import SvgCanvas from './components/SvgCanvas'
+import { PersonDetails } from './types'
+import { mapDetailsToSvg } from './utils/mapDetailsToSvg'
 
 function App (): ReactElement {
+  const details: Partial<PersonDetails> = {
+    emotion: 'sad',
+    cap: 'nocap',
+    gender: 'female',
+    glasses: 'glasses',
+    facialHair: 'beard'
+  }
+
+  const svgToRender = mapDetailsToSvg(details)
+
   return (
-    <div className='h-screen bg-red-600'>
+    <div>
       <h1>Notion Avatar Generator</h1>
+      <SvgCanvas svgList={svgToRender} />
     </div>
   )
 }
---
+++ src/utils/mapDetailsToSvg.ts
@@ -0,0 +1,18 @@
+import { PersonDetails } from '../types'
+import svgList from '../constants/svgList'
+
+export function getRandomCombination (combinations: string[][]): string[] {
+  return combinations[Math.floor(Math.random() * combinations.length)]
+}
+
+export function mapDetailsToSvg (details: Partial<PersonDetails>): string[] {
+  const entries = Object.entries(details)
+  const avatarSvgs = entries.flatMap(([key, value]) => {
+    const detail = svgList[key as keyof typeof svgList]
+
+    return getRandomCombination(detail[value as keyof typeof detail])
+  })
+
+  avatarSvgs.sort((a) => a.includes('head') ? -1 : 1)
+  return avatarSvgs
+}
