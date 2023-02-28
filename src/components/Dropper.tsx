b6e226c453afacdd2bf36b480d6d61ec61e8dfa0
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-02-28 02:23:04 -0300

    [FEATURE]: Create detection util and useDetection hook

--- src/components/Dropper.tsx
+++ src/components/Dropper.tsx
@@ -1,12 +1,15 @@
 import { ReactElement } from 'react'
 import Dropzone from 'react-dropzone'
 import { uploadImage } from '../utils/upload'
+import { useDetection } from '../hooks/useDetection'
 
 export default function Dropper (): ReactElement {
+  const { detection, setDetection } = useDetection()
+
   function handleDrop (droppedFile: any): void {
-    uploadImage(droppedFile[0]).then((res) => {
-      console.log('Dropper:', res)
-    }).catch(err => console.log(err))
+    uploadImage(droppedFile[0])
+      .then((response) => setDetection(response)).then(() => console.log('-->', detection))
+      .catch(err => console.log(err))
   }
 
   return (
---
+++ src/hooks/useDetection.ts
@@ -0,0 +1,29 @@
+import { getGlasses, getHighestEmotion, getHead, getFacialHair } from '../utils/detection'
+// import { useSignal } from '@preact/signals'
+// import { PersonDetails } from '../types'
+
+const initialState = {
+  gender: '',
+  emotion: '',
+  glasses: '',
+  head: '',
+  facial_hair: ''
+}
+
+export function useDetection (): any {
+  const detection = initialState
+
+  const setDetection = (detectionObject: any): void => {
+    if (detectionObject.info.detection.adv_face.data.length > 0) throw new Error('You must upload an individual image')
+
+    const data = detectionObject.info.detection.adv_face.data[0]
+
+    detection.gender = data.attributes.gender
+    detection.emotion = getHighestEmotion(data.attributes.emotion)
+    detection.glasses = getGlasses(data.attributes.glasses)
+    detection.head = getHead({ gender: data.attributes.gender, accessories: data.attributes.accessories, hair: data.attributes.hair })
+    detection.facial_hair = getFacialHair(data.attributes.facial_hair)
+  }
+
+  return { detection, setDetection }
+}
