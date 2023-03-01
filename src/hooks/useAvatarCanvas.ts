e9f85fc281a656299f650ce6a32621b979321551
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-01 20:14:47 -0300

    [UPDATE]: Fetch svgs after drawing

--- src/hooks/useAvatarCanvas.ts
+++ src/hooks/useAvatarCanvas.ts
@@ -1,21 +1,40 @@
-import { RefObject, useEffect, useRef } from 'react'
+import { RefObject, useEffect, useRef, useState } from 'react'
 
-export default function useCanvas (svgList: string[]): RefObject<HTMLCanvasElement> {
+export default function useAvatarCanvas (svgList: string[]): RefObject<HTMLCanvasElement> {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  console.log(svgList)
-  useEffect(() => {
-    const canvas = canvasRef.current
-    console.log(canvas)
-    const ctx = canvas.getContext('2d')
+  const [svgStrings, setSvgStrings] = useState<string[]>([])
 
-    svgList.forEach(svg => {
+  async function fetchSvg (svgList: string[]): Promise<string[]> {
+    const promises = svgList.map(async svg => await fetch(`./svgs/${svg}`))
+    const responses = await Promise.all(promises)
+
+    const textPromises = responses.map(async res => await res.text())
+    const strings = await Promise.all(textPromises)
+    return strings
+  }
+
+  useEffect(() => {
+    fetchSvg(svgList)
+      .then(strings => {
+        setSvgStrings(strings)
+      })
+      .catch(console.error)
+  }, [svgList])
+
+  useEffect(() => {
+    const canvas = canvasRef.current as HTMLCanvasElement
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+
+    svgStrings.forEach((string, index) => {
+      let finalSVG = string
+      if (index === 0) finalSVG = string.replace(/currentColor/g, '%23713F12')
       const img = new Image()
       img.onload = () => {
         ctx.drawImage(img, 0, 0)
       }
-      img.src = `./svgs/${svg}`
+      img.src = `data:image/svg+xml;charset=utf-8, ${finalSVG}`
     })
-  }, [svgList])
+  }, [svgStrings])
 
   return canvasRef
 }
