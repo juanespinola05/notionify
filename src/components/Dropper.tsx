dea841c27c0affe9a97d1581ab84433de8ae56fa
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-02-28 22:45:37 -0300

    [UPDATE]: Render details

--- src/components/Dropper.tsx
+++ src/components/Dropper.tsx
@@ -1,14 +1,22 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import Dropzone from 'react-dropzone'
 import { uploadImage } from '../utils/upload'
 import { useDetection } from '../hooks/useDetection'
+import SvgCanvas from './SvgCanvas'
+import { mapDetailsToSvg } from '../utils/mapDetailsToSvg'
 
 export default function Dropper (): ReactElement {
   const { detection, setDetection } = useDetection()
+  const [svgList, setSvgList] = useState<string[]>([])
 
   function handleDrop (droppedFile: any): void {
     uploadImage(droppedFile[0])
-      .then((response) => setDetection(response)).then(() => console.log('-->', detection))
+      .then((response) => setDetection(response))
+      .then(() => {
+        console.log('🚀 ~ file: Dropper.tsx:16 ~ .then ~ response:', detection)
+        const foo = mapDetailsToSvg(detection)
+        setSvgList(foo)
+      })
       .catch(err => console.log(err))
   }
 
@@ -22,6 +30,10 @@ export default function Dropper (): ReactElement {
           </div>
         )}
       </Dropzone>
+      <SvgCanvas svgList={svgList} />
+      <button onClick={() => setSvgList(mapDetailsToSvg(detection))}>
+        Regenerar
+      </button>
     </div>
   )
 }
