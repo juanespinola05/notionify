149f6d06c683931a49dedcec34d778978ccb8799
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-01 21:41:43 -0300

    [FEATURE]: Add downloadUrl and loading states (initial)

--- src/components/SvgCanvas.tsx
+++ src/components/SvgCanvas.tsx
@@ -1,16 +1,34 @@
 import { ReactElement } from 'react'
 import useAvatarCanvas from '../hooks/useAvatarCanvas'
-// import SvgLayer from './SvgLayer'
 
 interface IProps {
   svgList: string[]
+  width?: number
+  height?: number
 }
 
-function SvgCanvas ({ svgList }: IProps): ReactElement {
-  const canvasRef = useAvatarCanvas(svgList)
+function SvgCanvas ({ svgList, width = 306, height = 306 }: IProps): ReactElement {
+  const { canvasRef, downloadUrl, ready, loading } = useAvatarCanvas(svgList)
 
   return (
-    <canvas ref={canvasRef} width='306' height='306' />
+    <>
+      {loading && <p>Loading..</p>}
+      <canvas
+        ref={canvasRef}
+        width={width}
+        height={height}
+      />
+      {
+        ready && (
+          <a
+            href={downloadUrl}
+            download='avatar' className='block px-4 py-2 bg-blue-800 text-white'
+          >
+            Descargar Avatar
+          </a>
+        )
+      }
+    </>
   )
 }
 
--- src/hooks/useAvatarCanvas.ts
+++ src/hooks/useAvatarCanvas.ts
@@ -1,40 +1,53 @@
 import { RefObject, useEffect, useRef, useState } from 'react'
+import { fetchSvg } from '../utils/svg'
 
-export default function useAvatarCanvas (svgList: string[]): RefObject<HTMLCanvasElement> {
+const IMAGE_DOWNLOAD_FORMAT = 'image/png'
+interface AvatarCanvas {
+  downloadUrl: string | undefined
+  canvasRef: RefObject<HTMLCanvasElement>
+  loading: boolean
+  ready: boolean
+}
+
+export default function useAvatarCanvas (svgList: string[]): AvatarCanvas {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const [svgStrings, setSvgStrings] = useState<string[]>([])
-
-  async function fetchSvg (svgList: string[]): Promise<string[]> {
-    const promises = svgList.map(async svg => await fetch(`./svgs/${svg}`))
-    const responses = await Promise.all(promises)
-
-    const textPromises = responses.map(async res => await res.text())
-    const strings = await Promise.all(textPromises)
-    return strings
-  }
+  const [downloadUrl, setDownloadUrl] = useState('')
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    setLoading(true)
     fetchSvg(svgList)
-      .then(strings => {
-        setSvgStrings(strings)
-      })
-      .catch(console.error)
+      .then(strings => setSvgStrings(strings))
+      .catch(err => console.error(err))
+      .finally(() => setLoading(false))
   }, [svgList])
 
   useEffect(() => {
     const canvas = canvasRef.current as HTMLCanvasElement
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+    // reset canvas to avoid conflicts
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     svgStrings.forEach((string, index) => {
-      let finalSVG = string
-      if (index === 0) finalSVG = string.replace(/currentColor/g, '%23713F12')
+      const finalSVG = string
+      // if (index === 0) finalSVG = string.replace(/currentColor/g, '%23713F12')
       const img = new Image()
       img.onload = () => {
         ctx.drawImage(img, 0, 0)
       }
       img.src = `data:image/svg+xml;charset=utf-8, ${finalSVG}`
     })
+    // TODO: Fix this to not use interval
+    setTimeout(() => {
+      setDownloadUrl(canvas.toDataURL(IMAGE_DOWNLOAD_FORMAT))
+    }, 500)
   }, [svgStrings])
 
-  return canvasRef
+  return {
+    canvasRef,
+    loading,
+    downloadUrl,
+    ready: svgStrings.length > 0
+  }
 }
