cd2dcb69600b526993987fb24c15c6895d0244b2
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-05 18:49:04 -0300

    [FEATURE]: Create illustration context

--- src/context/illustration.tsx
+++ src/context/illustration.tsx
@@ -1,39 +1,94 @@
 import { useReducer, createContext, useContext } from 'react'
 
-const SET_ILLUSTRATIONS_IDS_LIST = 'SET_ILLUSTRATIONS_IDS_LIST'
+const BACKGROUND_ID = 'background'
+const DEFAULT_OUTFIT_ID = '5_outfit_1'
 
-const initialState = {
-  list: []
+const SET_ID_LIST = 'SET_ID_LIST'
+const TOGGLE_BACKGROUND_ID = 'TOGGLE_BACKGROUND_ID'
+const SET_OUTFIT_ID = 'SET_OUTFIT_ID'
+const SET_ACCESSORY_ID = 'SET_ACCESSORY_ID'
+
+interface State {
+  illustrationIdList: string[]
+  hasBackground: boolean
+  outfitId: string
+  accessoryId: string
 }
 
-function illustrationsReducer (state = initialState, action: any): any {
+const initialState: State = {
+  illustrationIdList: [],
+  hasBackground: true,
+  outfitId: DEFAULT_OUTFIT_ID,
+  accessoryId: ''
+}
+
+function illustrationsReducer (state: State, action: any): State {
   switch (action.type) {
-    case SET_ILLUSTRATIONS_IDS_LIST:
-      return { ...state, list: action.payload }
+    case SET_ID_LIST:
+      return { ...state, illustrationIdList: action.payload }
+    case TOGGLE_BACKGROUND_ID:
+      return { ...state, hasBackground: !state.hasBackground }
+    case SET_OUTFIT_ID:
+      return { ...state, outfitId: action.payload }
+    case SET_ACCESSORY_ID:
+      return { ...state, accessoryId: action.payload }
     default:
       return state
   }
 }
 
-const IllustrationsContext = createContext([])
+const IllustrationsContext = createContext<{ state: State, dispatch: React.Dispatch<any> }>({
+  state: initialState, dispatch: () => null
+})
 
 const IllustrationsProvider = ({ children }: any): JSX.Element => {
   const [state, dispatch] = useReducer(illustrationsReducer, initialState)
-  const value = { list: state.list, dispatch }
+  const value = { state, dispatch }
 
   return <IllustrationsContext.Provider value={value}> {children} </IllustrationsContext.Provider>
 }
 
-function useIllustrations (): any {
-  const context = useContext(IllustrationsContext)
-  if (context === undefined) throw new Error('useIllustrations must be used within a IllustrationsProvider')
-  const { dispatch }: any = context
+interface IllustrationsHook extends State {
+  setIllustrationsIDList: (list: string[]) => void
+  toggleBackground: () => void
+  setOutfit: (id: string) => void
+  setAccessory: (id: string) => void
+}
 
-  const setIllustrations = (list: string[]): void => {
-    dispatch({ type: SET_ILLUSTRATIONS_IDS_LIST, payload: list })
+function useIllustrations (): IllustrationsHook {
+  const { dispatch, state } = useContext(IllustrationsContext)
+  if (state === undefined) throw new Error('useIllustrations must be used within a IllustrationsProvider')
+  const { illustrationIdList, outfitId, accessoryId } = state
+
+  const list = illustrationIdList.length > 0
+    ? [outfitId, ...illustrationIdList, accessoryId, BACKGROUND_ID]
+    : []
+
+  const setIllustrationsIDList = (list: string[]): void => {
+    list.sort((a, b) => a.localeCompare(b))
+    dispatch({ type: SET_ID_LIST, payload: list })
   }
 
-  return { ...context, setIllustrations }
+  const toggleBackground = (): void => {
+    dispatch({ type: TOGGLE_BACKGROUND_ID })
+  }
+
+  const setOutfit = (id: string): void => {
+    dispatch({ type: SET_OUTFIT_ID, payload: id })
+  }
+
+  const setAccessory = (id: string): void => {
+    dispatch({ type: SET_ACCESSORY_ID, payload: id })
+  }
+
+  return {
+    ...state,
+    illustrationIdList: list,
+    setIllustrationsIDList,
+    toggleBackground,
+    setOutfit,
+    setAccessory
+  }
 }
 
 export { IllustrationsProvider, useIllustrations }
