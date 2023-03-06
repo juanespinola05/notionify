c2ebb9e9f2cf6635d139f77736ec77a8192c0f71
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-03-06 09:03:46 -0300

    [FEATURE] Create exportmodal

--- src/components/ExportModal.tsx
+++ src/components/ExportModal.tsx
@@ -1,26 +1,28 @@
 import { ReactNode, useState } from 'react'
+import useAvatarCanvas from '../hooks/useAvatarCanvas'
 
 interface ModalProps {
   label?: string | JSX.Element
   children?: ReactNode | ReactNode[]
 }
 
-const EXPORT_SIZES = ['300', '500', '1000']
-const EXPORT_FORMATS = ['PNG', 'JPG']
+const EXPORT_SIZES = ['300', '1000']
+const EXPORT_FORMATS = ['png', 'jpg']
 
 export default function ExportModal ({ label }: ModalProps): React.ReactElement {
   const [selectedSize, setSelectedSize] = useState('')
   const [selectedFormat, setSelectedFormat] = useState('')
+  const { handleDownload } = useAvatarCanvas()
 
   return (
     <>
-      <label htmlFor='my-modal-3' className='btn'>{label}</label>
+      <label htmlFor='my-modal-3' className='btn bg-gray-800'>{label}</label>
 
       <input type='checkbox' id='my-modal-3' className='modal-toggle' />
       <div className='modal'>
         <div className='modal-box relative'>
           <label htmlFor='my-modal-3' className='btn btn-sm btn-circle absolute right-2 top-2 rounded-xl'>✕</label>
-          <h3 className='text-lg font-bold '>Export avatar</h3>
+          <h3 className='text-lg font-bold '>Download avatar</h3>
 
           <div className='flex justify-between items-center pt-6 gap-6'>
             <div className='flex gap-4'>
@@ -39,7 +41,7 @@ export default function ExportModal ({ label }: ModalProps): React.ReactElement 
               }
             </div>
           </div>
-          <button disabled={!selectedSize || !selectedFormat} className='btn text-white mt-6 w-full border-none hover:bg-blue-400 bg-blue-400'>EXPORT</button>
+          <button disabled={!selectedSize || !selectedFormat} onClick={() => handleDownload({ format: selectedFormat, size: selectedSize })} className='btn text-white mt-6 w-full border-none hover:bg-blue-400 bg-blue-400'>Download</button>
 
         </div>
       </div>
