8abe18a5a42f12a8e356520da4bb0f65c1ffba90
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-07 01:49:17 -0300

    [UPDATE]: Update footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,24 +1,39 @@
-import { ReactElement } from 'react'
+import { ReactElement, ReactNode } from 'react'
 import { ReactComponent as BrandLogo } from '../assets/brand.svg'
+import { ReactComponent as CloudinaryLogo } from '../assets/cloudinary.svg'
+import MidudevLogo from '../assets/midudev.png'
 
 export default function Footer (): ReactElement {
   return (
-    <footer className='p-10 bg-black text-primary-content'>
-      <div className='flex flex-col gap-y-4'>
+    <footer className='bg-heroBackground text-primary-content'>
+      <div className='flex flex-col p-10 gap-y-4 items-center bg-[rgba(0,0,0,.85)]'>
         <div>
           <BrandLogo className='w-40' />
         </div>
         <hr className='bg-white w-full' />
-        <div>
-          <p className='font-bold'>
-            This tool was built for a Hackathon x&nbsp;
-            <a href='https://cloudinary.com' rel='noreferrer' className='underline'>Cloudinary</a>
-            &nbsp; & &nbsp;
-            <a href='https://midu.dev' rel='noreferrer' className='underline'>Midudev</a>
+        <div className='text-center'>
+          <p className='font-medium'>
+            This tool was built for Midudev's Hackathon
           </p>
-          <p>Copyright © 2023 - All right reserved</p>
+          <div className='flex gap-2 justify-center'>
+            <Link href='https://cloudinary.com'>
+              <CloudinaryLogo style={{ width: '100px' }} />
+            </Link>
+            <Link href='https://midu.dev'>
+              <img src={MidudevLogo} alt='' className='w-24' />
+            </Link>
+          </div>
+          <p className='mt-2 text-sm'>All credits to <Link href='https://www.drawkit.com/product/notion-style-avatar-creator'>DrawKit</Link> for set of illustrations</p>
         </div>
       </div>
     </footer>
+  )
+}
+
+function Link ({ children, href }: { children: ReactNode, href: string }): ReactElement {
+  return (
+    <a href={href} rel='noreferrer' className='underline underline-offset-4'>
+      {children}
+    </a>
   )
 }
